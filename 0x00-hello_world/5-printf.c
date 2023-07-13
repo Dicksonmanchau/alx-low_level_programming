82bb135aa3cbc057ed944333581b639008860310
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-13 13:32:19 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+ * main - a program that prints a line with puts function
+ * return 0 is equal  to success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
