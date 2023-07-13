d77607ccb6c8448acbed33d13847ed1cc4fcec65
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-13 13:46:23 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*
+ * main - a program that prints a line with puts function
+ * return 0 is equal  to success
+ */
+
+int main(void)
+{
+        char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(a));
+	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(b));
+	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(c));
+	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(d));
+	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(f));
+        return (0);
+}
