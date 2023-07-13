6ec7d0d3dbc5a6ca79d5a254fa0982cf694f5609
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-13 13:13:14 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/*
+ * main - a program that prints a line with puts function
+ * return 0 is equal  to success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
