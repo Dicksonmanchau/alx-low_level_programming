b1b039d7072a44efbf63aa5f41b1798a1ca7d16c
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-13 14:07:19 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-        char a;
+	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
-	prinntf("size of a char %lu byte(s)\n",( unsigned long) sizeof(a));
-	prinntf("size of a int %lu byte(s)\n",( unsigned long) sizeof(b));
-	prinntf("size of a long %lu byte(s)\n",( unsigned long) sizeof(c));
-	prinntf("size of a long long %lu byte(s)\n",( unsigned long) sizeof(d));
-	prinntf("size of a float %lu byte(s)\n",( unsigned long) sizeof(f));
-        return (0);
+	prinntf("size of a char %lu byte(s)\n", (unsigned long) sizeof(a));
+	prinntf("size of a int %lu byte(s)\n", (unsigned long) sizeof(b));
+	prinntf("size of a long %lu byte(s)\n", (unsigned long) sizeof(c));
+	prinntf("size of a long long %lu byte(s)\n", (unsigned long) sizeof(d));
+	prinntf("size of a float %lu byte(s)\n", (unsigned long) sizeof(f));
+	return (0);
 }
