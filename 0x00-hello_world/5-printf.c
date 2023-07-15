ce51fc40d4ca932f3dcb0e6456481aacc98b4c87
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-15 00:21:00 -0700

    include main decription

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*
- * main - a program that prints a line with puts function
- * return 0 is equal  to success
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description:
+ *   This function is the entry point of the program.
+ *   It prints a formatted string.
+ *   The return value of 0 indicates successful execution.
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
