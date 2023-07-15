fe02b0f3f5ee75bd71d0bae3c263f94f6b70b711
Dickson Ronko <dicksonmanchau@gmail.com>
2023-07-15 01:03:58 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
-/*
- * main - a program that prints a line with puts function
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates use of the sizeof operator to determine
+ * the size, in bytes, of different data types.
+ * It declares variables of various types:
+ * char,int,long int,long long int,and float. 
+ * It then uses the sizeof operator to retrieve the size of
+ * each variable and prints the results.
+ *
  * return 0 is equal  to success
  */
 
@@ -12,10 +21,17 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-	prinntf("size of a char %lu byte(s)\n", (unsigned long) sizeof(a));
-	prinntf("size of a int %lu byte(s)\n", (unsigned long) sizeof(b));
-	prinntf("size of a long %lu byte(s)\n", (unsigned long) sizeof(c));
-	prinntf("size of a long long %lu byte(s)\n", (unsigned long) sizeof(d));
-	prinntf("size of a float %lu byte(s)\n", (unsigned long) sizeof(f));
+	
+	/**
+	 * main - Entry point of the program
+	 *
+	 * return 0 is equal  to successful execution
+	 */
+
+	printf("size of a char %lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("size of a int %lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("size of a long %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("size of a long long %lu byte(s)\n", (unsigned long) sizeof(d));
+	printf("size of a float %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
